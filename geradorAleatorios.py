78d1754d1413e9db023afb0c1d83d4bf93e3ae95
Arthur <arthur.silva@aluno.faculdadeimpacta.com.br>
2021-09-01 21:21:38 -0300

    Alteração do valor do tamanho da lista e definição da lista para teste.

--- geradorAleatorios.py
+++ geradorAleatorios.py
@@ -5,7 +5,7 @@ def geradorAleatorios():
     listaNaoOrdenada = []
     
     # Tamanho da lista, conforme professor solicitou
-    tamanhoLista = 15
+    tamanhoLista = 16
     
     # Gera lista com numeros aleatórios e adiciona a lista
     for i in range(tamanhoLista):
@@ -13,3 +13,9 @@ def geradorAleatorios():
     return listaNaoOrdenada
 
 print(geradorAleatorios())
+print(geradorAleatorios())
+print(geradorAleatorios())
+
+# [32, 35, 65, 4, 4, 98, 29, 33, 18, 82, 74, 63, 57, 2, 1, 30]
+# [93, 37, 53, 97, 75, 28, 44, 60, 89, 0, 58, 95, 56, 32, 38, 14]
+# [37, 40, 70, 58, 26, 12, 69, 33, 9, 92, 14, 4, 24, 66, 23, 92]
