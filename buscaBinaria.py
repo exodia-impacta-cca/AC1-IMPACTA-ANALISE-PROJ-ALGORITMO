17c64a607b2401265d6d8d94c65916d02493b304
Arthur <arthur.silva@aluno.faculdadeimpacta.com.br>
2021-09-04 14:08:24 -0300

    Adição busca linear e binária.

--- buscaBinaria.py
+++ buscaBinaria.py
@@ -1,2 +1,23 @@
+from geradorAleatorios import l1,l2,l3
+
 # Implementação do algoritmo de busca binária
+def busca_binaria(lista, numero_procurado):
+    primeiro = 0
+    ultimo = len(lista) - 1
+    while primeiro <= ultimo:
+        meio = ((primeiro + ultimo) // 2)
+        if lista[meio] == numero_procurado:
+            return True
+        else:
+            if numero_procurado < lista[meio]:
+                ultimo = meio - 1
+            else:
+                primeiro = meio + 1
+    return False
+
 # Testes do algoritmo de busca binária
+print(l1)
+numero_procurado = int(input("Digite o número que deseja procurar na lista: "))
+
+resposta = busca_binaria(l1, numero_procurado)
+print("O número procurado está presente na lista?", resposta)
--- buscaLinear.py
+++ buscaLinear.py
@@ -1,2 +1,18 @@
+from geradorAleatorios import l1,l2,l3
+
 # Implementação do algoritmo de busca linear
+def busca_linear(lista, numero_procurado):
+    for i in range(len(lista)):
+        if lista[i] == numero_procurado:
+            return True
+    return False
+
+
 # Testes do algoritmo de busca linear
+print(l1)
+numero_procurado = int(input("Digite o número que deseja procurar na lista: "))
+
+if busca_linear(l1, numero_procurado):
+    print('Número encontrado!')
+else:
+    print('Não está na lista!')
